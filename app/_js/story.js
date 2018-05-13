96f9b2d23e9aa572353221a89a2ca914445bbe75
Carlos Matallín <carlos.matallin@transferwise.com>
2018-05-13 20:30:20 +0100

    Improve build

---
+++ app/_js/story.js
@@ -0,0 +1,31 @@
+import Scrollmap from './components/scroll-map.js';
+
+class Story {
+  constructor (options) {
+    this.scrollmapEl = options.scrollmapEl;
+    this.scrollmapFile = options.scrollmapFile;
+    this.scrollmapCenter = options.scrollmapCenter;
+  }
+
+  init () {
+    if (this.scrollmapEl) {
+      const scrollmap = new Scrollmap({
+        containerEl: this.scrollmapEl,
+        scrollmapFile: this.scrollmapFile,
+        scrollmapCenter: this.scrollmapCenter
+      });
+
+      scrollmap.init();
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const story = new Story({
+    scrollmapEl: document.querySelector('.js-Scroll'),
+    scrollmapFile: window.scrollmapFile,
+    scrollmapCenter: window.scrollmapCenter
+  });
+
+  story.init();
+});
