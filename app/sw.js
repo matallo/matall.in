7f21e72ea2e6412c9a3a68cf932833449b860994
Carlos Matallín <carlos.matallin@transferwise.com>
2019-01-17 01:53:07 +0000

    Add service worker

---
+++ app/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'matall.in-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/',
+  '/favicon.ico',
+  '/css/app.css',
+  '/js/app.js',
+  '/js/html5shiv.js',
+  '/js/polyfills.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }),
+      ),
+    ),
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    }),
+  );
+});
